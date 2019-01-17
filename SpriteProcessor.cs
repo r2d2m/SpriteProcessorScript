97a28847f4fc844ee4c8e3ce5fe3c9f1fbd86f86
Alex Belzer <37421414+dithyrambs@users.noreply.github.com>
2019-01-17 10:50:40 -0800

    Added  functionality
    Added support to specify different x and y values for tile size

--- SpriteProcessor.cs
+++ SpriteProcessor.cs
@@ -5,19 +5,22 @@ using UnityEditor;
 
 /// <summary>
 /// Note: Will only work when manually triggering "Reimport" on a file, not when importing a file to Unity for the first time.
-/// In order for this to work the grandparent directory must be names "AutoProcessedSpriteSheet"
-/// The immediate parent directory must be named the desired tile size of the sliced spritesheet in pixels, plus underscore, plus the desired pixels per unit
-///     eg. "16_100"
+/// In order for this to work the grandparent directory must be names "AutoProcessedSpriteSheets"
+/// The immediate parent directory must be named the desired horizontal tile size of the sliced spritesheet in pixels, plus underscore...
+/// plus the desired horizontal tile size of the sliced spritesheet in pixels, plus underscore...
+/// plus the desired pixels per unit
+///     eg. "16__12_100"
 /// </summary>
 public class SpriteProcessor : AssetPostprocessor
 {
     bool continueProcess = false;
     int ppu = 16;
-    int spriteSize = 16;
+    int spriteSizeX = 16;
+    int spriteSizeY = 16;
 
     void OnPreprocessTexture()
     {
-        if (Directory.GetParent(assetPath).Parent.Name != "AutoProcessedSpriteSheet") return;
+        if (Directory.GetParent(assetPath).Parent.Name != "AutoProcessedSpriteSheets") return;
 
         TextureImporter ti = (TextureImporter)assetImporter;
         ti.textureType = TextureImporterType.Sprite;
@@ -28,10 +31,11 @@ public class SpriteProcessor : AssetPostprocessor
         string[] parentDir = Directory.GetParent(assetPath).Name.Split('_');
 
         //If we can't parse the parent directory values as integers, don't continue processing the sprite
-        if ( !System.Int32.TryParse(parentDir[0], out spriteSize) && !System.Int32.TryParse(parentDir[1], out ppu) ) return;
-
-        ti.spritePixelsPerUnit = ppu;
-        continueProcess = true;
+        if ( System.Int32.TryParse(parentDir[0], out spriteSizeX) && System.Int32.TryParse(parentDir[1], out spriteSizeY) && System.Int32.TryParse(parentDir[2], out ppu) )
+        {
+            ti.spritePixelsPerUnit = ppu;
+            continueProcess = true;
+        }
     }
 
     public void OnPostprocessTexture(Texture2D texture)
@@ -39,10 +43,10 @@ public class SpriteProcessor : AssetPostprocessor
         //If we failed requirement during the PreProcess stage, return
         if (!continueProcess) return;
 
-        if ( spriteSize > 0 )
+        if ( spriteSizeX > 0 && spriteSizeY > 0)
         {
-            int colCount = texture.width / spriteSize;
-            int rowCount = texture.height / spriteSize;
+            int colCount = texture.width / spriteSizeX;
+            int rowCount = texture.height / spriteSizeY;
             string fileName = System.IO.Path.GetFileNameWithoutExtension(assetPath);
             int i = 0;
 
@@ -52,14 +56,15 @@ public class SpriteProcessor : AssetPostprocessor
             {
                 for (int c = 0; c < colCount; c++)
                 {
+                    Debug.LogWarning("spriteSizeY  " + spriteSizeY);
+
                     SpriteMetaData meta = new SpriteMetaData();
-                    meta.rect = new Rect(c * spriteSize, r * spriteSize, spriteSize, spriteSize);
+                    meta.rect = new Rect(c * spriteSizeX, r * spriteSizeY, spriteSizeX, spriteSizeY);
                     meta.name = fileName + "_" + i;
                     metas.Add(meta);
                     i++;
                 }
             }
-
             TextureImporter textureImporter = (TextureImporter)assetImporter;
             textureImporter.spritesheet = metas.ToArray();
 
